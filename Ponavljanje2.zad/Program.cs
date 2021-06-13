d3b317fda997b29a95ef39c3baafba5cb1aab516
markoknezovic <58472152+markoknezovic@users.noreply.github.com>
2021-06-13 20:54:45 +0200

    Add files via upload

---
+++ Ponavljanje2.zad/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponavljanje2.zad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("broj: ");
+                int broj = 100;
+                bool a = true;
+                KlasaD suprotnobool = new KlasaD();
+                KlasaD suprotnoint = new KlasaD();
+
+                Console.WriteLine("bool" + suprotnobool.suprotnobool(a) + ", broj" + suprotnoint.suprotnoint(broj));
+                Console.ReadLine();
+            }
+        }
+    }
+}
